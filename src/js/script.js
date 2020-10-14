17a5e03dce502f41554be906bdb4905a6dbb1da8
Naveen Kharwar <naveenkharwar@hotmail.com>
2020-10-14 18:23:05 +0530

    Structure Ready

---
+++ src/js/script.js
@@ -0,0 +1,110 @@
+import { CountUp } from "countup.js";
+
+$(".project-slider").slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+$(".team-slider").slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+$(".testimonial-slider").slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+});
+
+// CountUp Options
+const options = {
+  duration: 15,
+};
+
+// For awwards winning
+let satisfiedCustomers = new CountUp("satisfied-customers", 100, options);
+if (!satisfiedCustomers.error) {
+  satisfiedCustomers.start();
+} else {
+  console.error(satisfiedCustomers.error);
+}
+
+// For Projects Completed 
+let projectsCompleted = new CountUp("projects-completed", 1900, options);
+if (!projectsCompleted.error) {
+  projectsCompleted.start();
+} else {
+  console.error(projectsCompleted.error);
+}
+
+// For Hours Worked
+let hoursWorked = new CountUp("hours-worked", 509100, options);
+if (!hoursWorked.error) {
+  hoursWorked.start();
+} else {
+  console.error(hoursWorked.error);
+}
+
+// For Awwrds Winning
+let awwardsWinning = new CountUp("awwards-winning", 100, options);
+if (!awwardsWinning.error) {
+  awwardsWinning.start();
+} else {
+  console.error(awwardsWinning.error);
+}
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,11 +1,17 @@
 const mix = require("laravel-mix");
 
 mix
+  .js("src/js/script.js", "dist/js")
   .sass("src/scss/style.scss", "dist/css")
   .browserSync({
-      proxy: false,
-      port: '5000',
-      files: ["dist/css/style.css", "index.html"],
-      server: {baseDir: './'}
+    proxy: false,
+    port: "5000",
+    files: ["dist/css/style.css", "dist/js/script.js", "index.html"],
+    server: { baseDir: "./" },
   })
+  .copy('node_modules/slick-carousel/slick', 'dist/slick')
+  .copy('node_modules/jquery/dist', 'dist/js/jquery')
+  .copy('node_modules/countup.js/dist/countUp.min.js', 'dist/js/')
+  .copy('node_modules/@popperjs/core/dist/umd/popper.min.js', 'dist/js/')
+  .copy('node_modules/bootstrap/dist/js/bootstrap.min.js', 'dist/js/bootstrap')
   .setPublicPath("dist");
