71d4cc4d5436533e3bb68bdbd38e755df21a350a
Naveen Kharwar <naveenkharwar@hotmail.com>
2020-10-15 01:24:25 +0530

    Add copy images from src to dist

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,15 +3,19 @@ const mix = require("laravel-mix");
 mix
   .js("src/js/script.js", "dist/js")
   .sass("src/scss/style.scss", "dist/css")
+  .options({
+    processCssUrls: false,
+  })
   .browserSync({
     proxy: false,
     port: "5000",
     files: ["dist/css/style.css", "dist/js/script.js", "index.html"],
     server: { baseDir: "./" },
   })
-  .copy('node_modules/slick-carousel/slick', 'dist/slick')
-  .copy('node_modules/jquery/dist', 'dist/js/jquery')
-  .copy('node_modules/countup.js/dist/countUp.min.js', 'dist/js/')
-  .copy('node_modules/@popperjs/core/dist/umd/popper.min.js', 'dist/js/')
-  .copy('node_modules/bootstrap/dist/js/bootstrap.min.js', 'dist/js/bootstrap')
+  .copy("src/img", "dist/img")
+  .copy("node_modules/slick-carousel/slick", "dist/slick")
+  .copy("node_modules/jquery/dist", "dist/js/jquery")
+  .copy("node_modules/countup.js/dist/countUp.min.js", "dist/js/")
+  .copy("node_modules/@popperjs/core/dist/umd/popper.min.js", "dist/js/")
+  .copy("node_modules/bootstrap/dist/js/bootstrap.min.js", "dist/js/bootstrap")
   .setPublicPath("dist");
